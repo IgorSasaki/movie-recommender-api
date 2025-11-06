11ebbd85baffe037056236982c6366aadb79a890
IgorSasaki <igor-sasaki@hotmail.com>
2025-11-06 18:30:29 -0300

    Configure server and app file to aplication

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import { env } from './config/env.js';
+
+export const createApp = () => {
+  const app = express();
+
+  // 1) Middlewares globais
+  app.use(express.json());
+  app.use(helmet());
+  app.use(cors({ origin: env.corsOrigin })); // restrinja em produção
+  app.use(rateLimit({ windowMs: 60_000, max: 100 })); // throttle básico
+
+  // 2) Healthcheck
+  app.get('/health', (_req, res) => res.json({ ok: true }));
+
+  
+  return app;
+};
