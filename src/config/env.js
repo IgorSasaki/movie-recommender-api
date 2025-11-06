c55385aff9f3996dd16d8667b6cf5743e71d6194
IgorSasaki <igor-sasaki@hotmail.com>
2025-11-06 18:40:45 -0300

    Update environments variables

--- src/config/env.js
+++ src/config/env.js
@@ -2,8 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const env = {
+    nodeEnv: process.env.NODE_ENV ?? 'development',
     port: Number(process.env.PORT ?? 3333),
     jwtSecret: process.env.JWT_SECRET ?? 'change-me',
     jwtExpiresIn: process.env.JWT_EXPIRES_IN ?? '15m',
-    corsOrigin: process.env.CORS_ORIGIN ?? '*'
+    corsOrigin: process.env.CORS_ORIGIN ?? '*',
+    databaseUrl: process.env.DATABASE_URL ?? 'sqlite:./database.sqlite',
 };
